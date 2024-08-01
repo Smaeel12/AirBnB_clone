3e715c3ceaf605291f64112de8047fd61b4e21e7
Smaeel12 <ismailboubkri2002@gmail.com>
2024-08-01 19:12:04 +0000

    reload func

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,7 +14,7 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {} 
-    __avaliable_classes = {"BaseModel": BaseModel, "User": User}
+    __available_classes = {"BaseModel": BaseModel, "User": User}
 
     def all(self):
         """ Returns the dictionary __objects
@@ -24,25 +24,25 @@ class FileStorage:
     def new(self, obj):
         """ Sets in __objects the obj with key <obj class name>.id
         """
-        self.__objects.setdefault(f"{obj.__class__.__name__}.{obj.id}",
+        FileStorage.__objects.setdefault(f"{obj.__class__.__name__}.{obj.id}",
                                          obj)
 
     def save(self):
         """ Serializes __objects to the JSON file
         """
-        with open(self.__file_path, "w") as f:
+        with open(FileStorage.__file_path, "w") as f:
             json.dump({key: value.to_dict() for key, value in
-                       self.__objects.items()}, f)
+                       FileStorage.__objects.items()}, f)
 
     def reload(self):
         """ Deserializes the JSON file to __objects
         """
         try:
-            with open(FileStorage.__file_path, "r") as f:
+            with open(self.__file_path, "r") as f:
                 obj_dict = json.load(f)
                 for key, value in obj_dict.items():
                     cls_name = key.split('.')[0]
                     if cls_name in self.__available_classes:
                         self.__objects[key] = self.__available_classes[cls_name](**value)
-        except Exception:
+        except FileNotFoundError:
             pass
