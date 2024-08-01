2e0f3899962eb3b75d5e740fdadec14a1e667ee4
Smaeel12 <ismailboubkri2002@gmail.com>
2024-08-01 02:15:32 +0000

    7. Console 0.1

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 interpreter
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,6 +13,97 @@ class HBNBCommand(cmd.Cmd):
         prompt(str): prompt string
     """
     prompt = "(hbnb) "
+
+    # Create Command
+    def do_create(self, line):
+        """ Creates a new instance of BaseModel
+        """
+        if (line == 'BaseModel'):
+            obj = BaseModel()
+            obj.save()
+            print(obj.id)
+        elif (len(line) == 0):
+            print("** class name missing **")
+        else:
+            print("** class doesn't exist **")
+
+    # Create Command
+    def do_show(self, line):
+        """ Prints the string representation of an instance based on the class
+        name and id
+        """
+        cmd_p = line.split()
+        if (len(cmd_p) != 2):
+            if (len(cmd_p) == 0):
+                print("** class name missing **")
+            if (len(cmd_p) == 1):
+                if (cmd_p[0] != 'BaseModel'):
+                    print("** class doesn't exist **")
+                else:
+                    print("** instance id missing **")
+        else:
+            if (storage.all().get(f'BaseModel.{cmd_p[1]}')):
+                print(storage.all().get(f'BaseModel.{cmd_p[1]}'))
+            else:
+                print("** no instance found **")
+
+    # Create Command
+    def do_destroy(self, line):
+        """ Deletes an instance based on the class name and id
+        """
+        cmd_p = line.split()
+        if (len(cmd_p) != 2):
+            if (len(cmd_p) == 0):
+                print("** class name missing **")
+            if (len(cmd_p) == 1):
+                if (cmd_p[0] != 'BaseModel'):
+                    print("** class doesn't exist **")
+                else:
+                    print("** instance id missing **")
+        else:
+            if (storage.all().get(f'BaseModel.{cmd_p[1]}')):
+                storage.all().pop(f'BaseModel.{cmd_p[1]}')
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    # Create Command
+    def do_all(self, line):
+        """ Prints all string representation of all instances based or not on
+        the class name
+        """
+        if (line == 'BaseModel' or len(line) == 0):
+            obj_list = [str(obj) for obj in storage.all().values()]
+            print(obj_list)
+        else:
+            print("** class doesn't exist **")
+
+    # Create Command
+    def do_update(self, line):
+        """ Updates an instance based on the class name and id by adding or
+        updating attribute
+        """
+        cmd_p = line.split()
+        if (len(cmd_p) == 0):
+            print("** class name missing **")
+            return
+        if (len(cmd_p) == 1):
+            if (cmd_p[0] != 'BaseModel'):
+                print("** class doesn't exist **")
+            else:
+                print("** instance id missing **")
+            return
+        if (len(cmd_p) == 2):
+            if not storage.all().get(f'BaseModel.{cmd_p[1]}'):
+                print("** no instance found **")
+            else:
+                print("** attribute name missing **")
+            return
+        if (len(cmd_p) == 3):
+            print("** value missing **")
+            return
+        obj = storage.all().get(f'BaseModel.{cmd_p[1]}')
+        setattr(obj, cmd_p[2], cmd_p[3])
 
     # Emptyline Handling
     def emptyline(self):
