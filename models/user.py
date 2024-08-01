86b190832ad185045f0203e4218d747104284453
Smaeel12 <ismailboubkri2002@gmail.com>
2024-08-01 16:59:26 +0000

    last checks 2

--- models/user.py
+++ models/user.py
@@ -1,10 +1,19 @@
-""" This module contains the User class
-"""
+#!/usr/bin/python3
+"""Defines the User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """
+    User class that inherits from BaseModel.
+    """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Constructor for User class.
+        """
+        super().__init__(*args, **kwargs)
