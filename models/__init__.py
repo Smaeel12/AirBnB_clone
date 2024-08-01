ca696e58818c276879bb3706061d9170d9351f6f
Smaeel12 <ismailboubkri2002@gmail.com>
2024-08-01 16:56:40 +0000

    last checks

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
+"""
+This script initializes a storage system for an application and loads existing
+data if available.
+"""
 from models.engine.file_storage import FileStorage
+from models.user import User
+
 
 storage = FileStorage()
 storage.reload()
